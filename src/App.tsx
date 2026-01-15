6f04ff99e198e80d0ee4fbaab28bbcae3d7522cc
Rehan Aslam <rabscs@gmail.com>
2026-01-15 17:43:22 +0500

    feat: include all compoements to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useContext } from "react";
+import { BasketContext } from "./context/createBasketContext";
+import { PRODUCTS } from "./basket/config";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const context = useContext(BasketContext);
+  
+  if (!context) {
+    throw new Error("App must be used within BasketProvider");
+  }
+  
+  const { dispatch, total } = context;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <h1>Acme Widget Co</h1>
 
-export default App
+      <div className="products">
+        {Object.values(PRODUCTS).map(product => (
+          <button
+            key={product.code}
+            onClick={() =>
+              dispatch({ type: "ADD_ITEM", payload: product.code })
+            }
+          >
+            {product.name} (${product.price})
+          </button>
+        ))}
+      </div>
+
+      <div className="total">Total: ${total}</div>
+    </div>
+  );
+}
