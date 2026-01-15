1e81180ae469b366ac83aa41ef6532d2cd1b24fa
Rehan Aslam <rabscs@gmail.com>
2026-01-15 17:38:39 +0500

    feat: add proper products and delivery rules

---
+++ src/basket/config.ts
@@ -0,0 +1,16 @@
+import { type Product, type DeliveryRule } from "./Basket.js";
+import { buyOneGetSecondHalfPrice } from "./offers.js";
+
+export const PRODUCTS: Record<string, Product> = {
+  R01: { code: "R01", name: "Red Widget", price: 32.95 },
+  G01: { code: "G01", name: "Green Widget", price: 24.95 },
+  B01: { code: "B01", name: "Blue Widget", price: 7.95 }
+};
+
+export const DELIVERY_RULES: DeliveryRule[] = [
+  { minTotal: 90, charge: 0 },
+  { minTotal: 50, charge: 2.95 },
+  { minTotal: 0, charge: 4.95 }
+];
+
+export const OFFERS = [buyOneGetSecondHalfPrice("R01")];
